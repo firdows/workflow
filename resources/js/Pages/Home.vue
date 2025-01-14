22369e58b247cbf77ae04196378998dba5bf058a
Ake.A <ahamad.jehduaramae@ssup.co.th>
2025-01-15 00:24:36 +0700

    add title

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,12 +1,14 @@
 <script setup>
-  import Layout from "../Layouts/Layout.vue";
-  const test = 'Component';
-  
+import Layout from "../Layouts/Layout.vue";
+import { Head } from "@inertiajs/vue3";
+const test = "Component";
+
 defineOptions({ layout: Layout });
 </script>
 
-
 <template>
-    <h1 class="text-success">Hello World {{test}}1</h1>
-  
+  <Head>
+    <title>Home</title>
+  </Head>
+  <h1 class="text-success">Hello World {{ test }}1</h1>
 </template>
