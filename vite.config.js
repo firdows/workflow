ec45ee0624f701a86fae5793408d6b73815dff0b
Ake.A <ahamad.jehduaramae@ssup.co.th>
2025-01-15 01:54:38 +0700

    build for deploy to server

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    // base: "./",
+    // build: {
+    //     outDir: 'dist',
+    //     assetsDir: 'assets',
+    //   },
+    // server: {
+    //     https: true,
+    //     host: 'dev.baansanfun.com',
+    // },
     plugins: [
         vue(),
         laravel({
